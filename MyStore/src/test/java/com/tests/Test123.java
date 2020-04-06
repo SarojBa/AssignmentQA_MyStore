7007dcc45814ff59e57fecdf30d24a5398541e39
varunkushwaha76 <varunkushwaha76@gmail.com>
2020-04-06 21:44:26 +0530

    Changes done

--- MyStore/src/test/java/com/tests/Test123.java
+++ MyStore/src/test/java/com/tests/Test123.java
@@ -7,8 +7,7 @@ public class Test123 {
 		System.out.println("This is new test class ");
 		System.out.println(" added on 25 march ");
 		System.out.println(" added on 27 march ");
-		System.out.println(" added on 27 march ");
-		System.out.println(" New modification done on  06 April ");
+		System.out.println(" New modification done on 06 April.. ");
 	}
 
 }
